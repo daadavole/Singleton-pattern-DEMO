83503dda76fbaf45a25fe1e1ea20983f035fd56f
daadavole <dostaldavid@post.cz>
2023-11-27 13:25:24 +0100

    Implemented two Singleton versions

--- Program.cs
+++ Program.cs
@@ -2,15 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Singleton_pattern_DEMO
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Non safe Singleton test");
+            //Testing the non thread safe Singleton version
+            Singleton_non_thread_safe s1 = Singleton_non_thread_safe.GetInstance();
+            Singleton_non_thread_safe s2 = Singleton_non_thread_safe.GetInstance();
 
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
+            //Testing the thread safe Singleton version
+            Console.WriteLine("Safe Singleton test");
+            Console.WriteLine(
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+            Console.ReadKey();
+        }
+        public static void TestSingleton(string value)
+        {
+            Singleton_thread_safe singleton = Singleton_thread_safe.GetInstance(value);
+            Console.WriteLine(singleton.Value);
         }
     }
 }
---
+++ Singleton non thread safe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_pattern_DEMO
+{
+    // Bad code! Do not use!
+
+    // The Singleton should always be a 'sealed' class to prevent class
+    // inheritance through external classes and also through nested classes.
+    public sealed class Singleton_non_thread_safe
+    {
+        private static Singleton_non_thread_safe _instance = null;
+
+        // The Singleton's constructor should always be private to prevent
+        // direct construction calls with the `new` operator.
+        private Singleton_non_thread_safe()
+        {
+        }
+
+        // The Singleton's instance is stored in a static field.
+        public static Singleton_non_thread_safe GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton_non_thread_safe();
+            }
+            return _instance;
+        }
+    }
+}
